9f244c057f794ca89d1b3680abb6330d09485d68
nmbarkade <nmbarkade@gmail.com>
2022-03-10 06:54:49 +0530

    Add revised code for odd number

--- welcome.py
+++ welcome.py
@@ -4,4 +4,4 @@ num = int (input ("Enter any number to test whether
 if (num % 2) == 0:
               print ("The number is even")
 else
-         print ("The number is odd") // odd statement
+         print ("The number is odd")
